8ee2597add8a6c01bdf0d5fa82a47b603920c71d
MacGyver <macgyver1024@gmail.com>
2018-02-09 22:44:59 +0100

    Fullscreen image background, rename images

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  static selectMusic() {
+    const selectedMusic = "chopin-spring";
+    return selectedMusic;
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentMusic: App.selectMusic()
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App"
+        style={{backgroundImage: "url(background/" + this.state.currentMusic + ".jpg)"}}>
       </div>
     );
   }
